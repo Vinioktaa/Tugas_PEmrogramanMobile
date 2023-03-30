b6a8293169d06ce71c5086683bf0d6f928248dec
Vinioktaa <115062431+Vinioktaa@users.noreply.github.com>
2023-03-30 12:06:42 +0700

    Add files via upload

---
+++ lib/data/news_data.dart
@@ -0,0 +1,55 @@
+import 'package:navigator_apps/models/news.dart';
+
+List<News> newsData = [
+  News(
+    id: 1,
+    title: "Masjid Kubah Emas Kota Depok",
+    description:
+        "Tempat ibadah umat Islam ini, menjadi salah satu ikon wisata yang indah dan banyak dikunjungi di Kota Depok. Masjid ini mampu menampung sekitar kurang lebih 20.000 jamaah dengan luas 8.000 meter persegi. Kawasan sekitar masjid seringkali disebut sebagai kawasan masjid termegah di Asia Tenggara.Selain pengunjung dapat beribadah dan memuja kebesaran-Nya, mereka juga bisa mengabadikan momen dengan berswafoto di halaman Masjid Kubah Emas. Masjid buatan Hj. Dian Djuriah Maimun Al-Rasyid ini merepresentasikan bangunan megah di dalam masjid sebagai bukti kebesaran Allah SWT.",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-9.jpeg?w=2560",
+  ),
+  News(
+    id: 2,
+    title: "Pantai Batu Karas",
+    description:
+        "Mengunjungi Pangandaran, wisatawan bisa menikmati keindahan pantai sambil berenang, surfing, ataupun wisata kuliner di Pantai Batu Karas. Pantai ini terbilang masih cukup sepi, sehingga nyaman untuk dikunjungi.Pantai Batu Karas terkenal dengan ombak yang cukup besar, sehingga tak heran jika pantai ini banyak direkomendasikan komunitas peselancar ombak sebagai arena berselancar yang nyaman jika ingin berwisata di Jawa Barat. ",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-8_169.jpeg?w=620",
+  ),
+  News(
+    id: 3,
+    title: "Gua Sunyaragi",
+    description:
+        "Kota Cirebon memiliki salah satu tempat wisata di Jawa Barat yang cukup legendaris. Gua Sunyaragi atau Taman Sari Sunyaragi merupakan sebuah situs bersejarah. Dahulu, Para Sultan menggunakan tempat ini sebagai tempat bermeditasi bagi para anggota kerajaan Keraton Kasepuhan Cirebon serta mengatur taktik perlawanan kepada para penjajah Belanda. Di dalam kompleksnya terdapat kolam air yang berfungsi sebagai pendingin ruangan dalam gua. Dalam komplek Gua Sunyaragi tidak terdapat bangunan masjid, melainkan hanya ada kamar-kamar mirip ruangan bawah tanah yang merupakan tempat ibadah dengan ukuran ruangan yang kecil. Selain menyimpan sejarah, mitos terucap dari mulut ke mulut terkait Gua ini. Salah satu mitosnya yakni konon jika seorang gadis menyentuh Patung Perawan Sunti, maka ia akan sulit mendapatkan jodoh.",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-7.jpeg?w=768",
+  ),
+  News(
+    id: 4,
+    title: "Situs Megalitikum Gunung Padang",
+    description:
+        "Tak hanya sejarah dan keindahan alam, Jawa Barat juga menyimpan banyak keajaiban. Salah satunya Situs Megalitikum Gunung Padang yang ditemukan pada tahun 1914. Bahkan oleh beberapa sejarawan menyebutkan gunung ini sebagai situs tertua di dunia mengalahkan Piramida Gaza di Mesir. Dengan kapasitas kurang lebih 9.000 orang, kompleks utamanya memiliki luas 900 m². Fungsi situs ini sebelum menjadi tempat wisata, diperkirakan menjadi tempat pemujaan bagi masyarakat setempat pada tahun sekitar 2000 SM..",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-6.jpeg?w=2560",
+  ),
+  News(
+    id: 5,
+    title: "Curug Cikondang",
+    description:
+        "Tak terhitung ada berapa banyak destinasi wisata air di Tanah Sunda, dan kali ini salah satu air terjun yang sangat mengagumkan berada di Kabupaten Cianjur. Menjajakan panorama menakjubkan dari niagara mini versi bumi Parahyangan, Curug Cikondang jadi salah satu primadona di Jawa Barat. Curug Cikondang punya julukan Niagara Mini, ini muncul karena sekilas, air terjun ini mirip dengan air terjun raksasa Niagara di Amerika Utara. Bentuknya melebar sepanjang 30 meter dengan tinggi 50 meter. Keunikan air terjun ini terdapat pada ciri khas adanya beberapa air terjun kecil sehingga menambah keindahan panorama.",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-5.jpeg?w=1024",
+  ),
+];
+
+List<News> hotNews = [
+  News(
+    id: 4,
+    title: "Curug Ibun Majalengka",
+    description:
+        "Penasaran dengan surga tersembunyi di Majalengka? Baiknya sempatkan berkunjung ke Curug Ibun. Dijuluki sebagai Green Canyon versi Majalengka, keindahannya tak bisa dielakkan lagi. Letaknya berada di antara dua tebing tinggi, berpadu dengan keindahan air terjun dan jernihnya aliran sungai. Pastinya, tempat wisata ini sering ramai dikunjungi, terutama pada pagi dan sore hari. Kebanyakan wisatawan ingin memperoleh udara segar dan dinginnya air di pagi hari, atau mengabadikan momen berfoto saat cahaya matahari sampai pada kemiringan yang sempurna dan menembus aliran sungai. ",
+    image:
+        "https://akcdn.detik.net.id/community/media/visual/2022/07/13/wisata-di-jawa-barat-4.jpeg?w=2560",
+  ),
+];
---
+++ lib/models/user.dart
@@ -0,0 +1,29 @@
+class User {
+  final int id;
+  final String name;
+  final String userName;
+  final String email;
+  final String? profileImage;
+  final String? phoneNumber;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.userName,
+    required this.email,
+    this.profileImage,
+    this.phoneNumber,
+  });
+
+  factory User.dummy() {
+    return User(
+      id: 027,
+      name: "Vini OKtapiani",
+      userName: "VIniok",
+      email: "2006059@itg.ac.id",
+      profileImage:
+          "https://asset.kompas.com/crops/ncgvDkq11ovx_624dxbv483x_iY=/0x0:648x432/750x500/data/photo/2021/10/05/615c371c61b81.jpg",
+      phoneNumber: "083176215809",
+    );
+  }
+}
